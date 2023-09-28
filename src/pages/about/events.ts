5710e60718cde6e5ba20be66049deb158370e027
SampoLS <mr.sampo@outlook.com>
2023-09-28 12:26:21 +0800

    updated;

--- src/pages/about/events.ts
+++ src/pages/about/events.ts
@@ -1,4 +1,4 @@
-export function showMask(clsName: string, j: number) {
+export const showMask = (clsName: string, j: number)=> {
     const el = document.getElementsByClassName(clsName) as HTMLCollectionOf<HTMLElement>;
     for (let i = 0; i < el.length; i++) {
         if (i === j) {
@@ -7,7 +7,7 @@ export function showMask(clsName: string, j: number) {
     }
 }
 
-export function hideMask(clsName: string) {
+export const hideMask = (clsName: string) => {
     const el = document.getElementsByClassName(clsName) as HTMLCollectionOf<HTMLElement>;
     for (let i = 0; i < el.length; i++) {
         el[i].style.opacity = '0';
@@ -17,14 +17,15 @@ export function hideMask(clsName: string) {
 export const mouseEnterFamiliar = (j: number) => {
     showMask('familiar', j);
 }
+
 export const mouseLeaveFamiliar = () => {
     hideMask('familiar');
 }
+
 export const mouseEnterUnfamiliar = (j: number) => {
     showMask('unfamiliar', j);
 };
+
 export const mouseLeaveUnfamiliar = () => {
     hideMask('unfamiliar');
 };
-
-
